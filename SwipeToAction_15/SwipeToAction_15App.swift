513fae3984fc21feea334dbf129922c4e4934701
emm <723125@gmail.com>
2021-01-26 11:28:02 +0000

    Initial Commit

---
+++ SwipeToAction_15/SwipeToAction_15App.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeToAction_15App.swift
+//  SwipeToAction_15
+//
+//  Created by emm on 26/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwipeToAction_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
